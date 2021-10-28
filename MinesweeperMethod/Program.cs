e19caf08cdd4c1ec1f8804d075e72ba22365f5a1
Åsa Roshällen <asa.maria.forss@gmail.com>
2021-10-28 19:04:54 +0200

    Liten ändring på Main metoden

--- MinesweeperMethod/Program.cs
+++ MinesweeperMethod/Program.cs
@@ -7,7 +7,7 @@ namespace MinesweeperMethod
         static void Main(string[] args)
         {
             char[,] start = new char[,] { { 'E', 'E', 'E', 'E', 'E' }, { 'E', 'E', 'M', 'E', 'E' }, { 'E', 'E', 'E', 'E', 'E' }, { 'E', 'E', 'E', 'E', 'E' } };
-            char[,] after = new char[,] { { '0', '1', 'E', '1', '0' }, { '0', '1', 'M', '1', '0' }, { '0', '1', '1', '1', '0' }, { '0', '0', '0', '0', '0' } };
+           
             int col = 0;
             int row = 3;
             char[,] fromCall;
